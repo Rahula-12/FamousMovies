d706a03e9ed0583ad0d66745f9038a7b0114434c
rahul <ra9853044@gmail.com>
2023-11-10 01:17:51 +0530

    successfully made network request

--- app/src/main/java/com/example/famousmovies/FamousMoviesApp.kt
+++ app/src/main/java/com/example/famousmovies/FamousMoviesApp.kt
@@ -1,29 +1,44 @@
 package com.example.famousmovies
 
+import android.util.Log
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.famousmovies.screens.DetailScreen
 import com.example.famousmovies.screens.MainScreen
+import com.example.famousmovies.viewModel.MoviesViewModel
 
 enum class Screen{
     MAINSCREEN,DETAILSCREEN
 }
 @Composable
 fun FamousMoviesApp(
+    viewModel: MoviesViewModel,
     modifier:Modifier=Modifier
 ) {
+    val moviesState=viewModel.moviesState.collectAsState()
+    Log.d("Movies",moviesState.value.list.size.toString())
     val navController= rememberNavController()
     NavHost(
         navController = navController,
         startDestination = Screen.MAINSCREEN.name
     ){
         composable(Screen.MAINSCREEN.name) {
-            MainScreen()
+            MainScreen(
+                onCardClicked = {
+                    navController.navigate(Screen.DETAILSCREEN.name)
+//                    Log.d("Test","I was pressed")
+                },
+                viewModel = viewModel
+            )
         }
         composable(Screen.DETAILSCREEN.name) {
-
+            DetailScreen(
+                viewModel=viewModel
+            )
         }
     }
 }
