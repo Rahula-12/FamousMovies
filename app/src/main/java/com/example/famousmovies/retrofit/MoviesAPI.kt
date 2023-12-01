7bb3238884d674c55170ab416d3a2433882a7444
rahul <ra9853044@gmail.com>
2023-12-01 23:28:27 +0530

    built api service interface

---
+++ app/src/main/java/com/example/famousmovies/retrofit/MoviesAPI.kt
@@ -0,0 +1,12 @@
+package com.example.famousmovies.retrofit
+
+import com.example.famousmovies.model.MovieItem
+import retrofit2.http.GET
+import retrofit2.http.Headers
+interface MoviesAPI {
+
+    @GET("/")
+    @Headers("X-RapidAPI-Key:1e59c5254cmsh65f87366aa4844fp138d18jsn4df36025d134,X-RapidAPI-Host:imdb-top-100-movies.p.rapidapi.com")
+    suspend fun getMovies():List<MovieItem>
+
+}
