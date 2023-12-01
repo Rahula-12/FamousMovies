889f7ac959928dd1da5b0b6ab21c86be53fb2130
rahul <ra9853044@gmail.com>
2023-12-01 23:09:53 +0530

    made MovieItem class

---
+++ app/src/main/java/com/example/famousmovies/model/MovieItem.kt
@@ -0,0 +1,16 @@
+package com.example.famousmovies.model
+
+data class MovieItem(
+    val big_image: String,
+    val description: String,
+//    val genre: List<String>,
+    val id: String,
+//    val image: String,
+//    val imdb_link: String,
+//    val imdbid: String,
+    val rank: Int,
+    val rating: String,
+//    val thumbnail: String,
+    val title: String,
+    val year: Int
+)
